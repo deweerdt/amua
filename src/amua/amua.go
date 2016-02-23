6f0014d7a486e5f70ed05fba3f7c3616a7156e08
Frederik Deweerdt <frederik.deweerdt@gmail.com>
2016-02-22 19:55:35 -0800

    Add Ctrl+F and Ctrl+P to scroll up and down in the maildir view

--- src/amua/amua.go
+++ src/amua/amua.go
@@ -549,7 +549,9 @@ func keybindings(amua *Amua, g *gocui.Gui) error {
 			{gocui.KeyArrowUp, maildir_move(-1), false},
 			{'j', maildir_move(1), false},
 			{gocui.KeyArrowDown, maildir_move(1), false},
+			{gocui.KeyCtrlF, maildir_move(10), false},
 			{gocui.KeyPgdn, maildir_move(10), false},
+			{gocui.KeyCtrlP, maildir_move(-10), false},
 			{gocui.KeyPgup, maildir_move(-10), false},
 		},
 		MESSAGE_VIEW: {
